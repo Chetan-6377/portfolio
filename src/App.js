1b3154be904b940d71ded2f7f63665a0de678099
Chetan-6377 <86248022+Chetan-6377@users.noreply.github.com>
2021-09-09 09:54:11 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './Header'
 import MyWork from './MyWork'
 import Nav from './nav'
 import WhatIDo from './WhatIDo'
+import SyncLoader from "react-spinners/SyncLoader";
 
-const App=()=>{
-    return<>
-    <Nav/>
-    <Header/>
-    <MyWork/>
-    <WhatIDo/>
-    </>
+
+const App = () => {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 4000);
+    }, [])
+    return (
+        <>
+            {
+                loading ?
+                <div className="app">
+                    <SyncLoader color={"#fff"} loading={loading} size={20} />
+                </div>
+                    :
+                    <>
+                        <Nav />
+                        <Header />
+                        <MyWork />
+                        <WhatIDo />
+
+                    </>
+            }
+
+        </>
+    )
 
 }
 export default App
